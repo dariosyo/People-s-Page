6021b271c8051454672f7fb3d698d6a3c4b54730
Da.T <38336173+dariosyo@users.noreply.github.com>
2021-05-01 14:55:09 -0700

    final v.1 code complete

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const $ = require('jquery');
+var _ = require('lodash');
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
+const featureNotAvailable = "This feature will be available on the next Version. Sorry for the inconvenience. Thank you. "
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -68,6 +70,12 @@ app.get("/", function(req, res) {
 app.get("/about", function(req, res) {
   res.render("about", {
     aboutContent: aboutContent
+  });
+});
+
+app.get("/login", function(req, res){
+  res.render("login", {
+    aboutContent:featureNotAvailable 
   });
 });
 
@@ -178,6 +186,20 @@ app.get("/educational", function(req, res) {
   });
 });
 
+// app.get("/posts/:post_id", function(req,res){
+//   const reqTitle = _.lowerCase(req.params.postName);
+//
+//   posts.forEach(function(post){
+//     const storedTitle = _.lowerCase(post.title);
+//
+//     if(reqTitle === storedTitle){
+//       res.render("post", {
+//         title:post.title,
+//         content:post.content
+//       })
+//     }
+//   })
+// })
 
 //-------------------Compose route---------------
 
@@ -243,7 +265,7 @@ const today = new Date();
 function dayFunction() {
   // const today = new Date();
   let options = {
-    weekday: "long",
+    weekday: "short",
     day: "numeric",
     month: "long",
     year: 'numeric'
