51287f0211a406877507803e4967b7a8713fc071
Da.T <38336173+dariosyo@users.noreply.github.com>
2021-05-04 18:55:21 -0700

    updated app listen port

--- app.js
+++ app.js
@@ -472,13 +472,17 @@ app.post("/healthDelete", function(req, res) {
 });
 
 // heroku deploy setup
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
+// app.listen(port);
+//
+//
+// app.listen(port, function() {
+//   console.log("Server has started.");
+// });
 
-
-app.listen(port, function() {
-  console.log("Server has started.");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000.");
 });
